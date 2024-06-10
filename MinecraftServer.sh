e333ccefe1fd1d507d3762a54de1fd4eb60cb5cd
mcdonaez <92563275+mcdonaez@users.noreply.github.com>
2024-06-10 11:06:24 -0700

    Update MinecraftServer.sh
    Corrected my EULA error and switched to Spigot instead of the default server.jar

--- MinecraftServer.sh
+++ MinecraftServer.sh
@@ -9,12 +9,17 @@ sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto
 sudo yum install -y java-21-amazon-corretto-devel.x86_64
 
 # Download server
-sudo curl -O https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
+#sudo curl -O https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
+sudo curl -O https://download.getbukkit.org/spigot/spigot-1.20.6.jar
+
 # Start the server for file generation
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+java -Dcom.mojang.eula.agree=true -Xmx3G -Xms2G -jar spigot-1.20.6.jar nogui
 
 # Accept the Minecraft eula
 sudo echo "eula=true" > eula.txt
+
+# Move the eula.txt to the correct location
+sudo mv eula.txt /home/ec2-user/eula.txt
 
 # Create a service file for server
 sudo tee /etc/systemd/system/minecraft.service << EOT
@@ -25,7 +30,7 @@ After=network.target
 
 [Service]
 User=ec2-user
-ExecStart=/usr/bin/java -Xmx1024M -Xms1024M -jar /home/ec2-user/server.jar nogui
+ExecStart=/usr/bin/java -Dcom.mojang.eula.agree=true -Xmx3G -Xms2G -jar /home/ec2-user/spigot-1.20.6.jar nogui
 ExecStop=stop
 WorkingDirectory=/home/ec2-user
 Restart=always
